49b9355ac172dca422428033126d6377e4aea8ef
Tushar Khan <tushar.54k@gmail.com>
2020-12-17 07:22:56 -0500

    colored interpreter output

--- src/main.ml
+++ src/main.ml
@@ -1,53 +1,88 @@
+let eof_error_msg () =
+  ANSITerminal.(print_string [red] "Unexpected EOF\n")
+
 let syntax_error_msg lexbuf =
-  if lexbuf.Lexing.lex_eof_reached
-  then Format.printf "Unexpected EOF"
-  else 
-    let p = lexbuf.Lexing.lex_curr_p in
-    let file = p.Lexing.pos_fname in
-    let row = p.Lexing.pos_lnum in
-    let col = p.Lexing.pos_cnum in
+  (* lexing position info *)
+  let p_s = lexbuf.Lexing.lex_start_p in
+  let p_e = lexbuf.Lexing.lex_curr_p in
+  (* error span position info *)
+  let file = p_s.Lexing.pos_fname in
+  let row = p_s.Lexing.pos_lnum in
+  let span_s = (p_s.Lexing.pos_cnum - p_s.Lexing.pos_bol) in
+  let span_e = (p_e.Lexing.pos_cnum - p_e.Lexing.pos_bol) in
 
-    let rec apply f x n = 
-      if n = 0 then x else apply f (f x) (n-1) 
-    in
+  (* helper function *)
+  let rec apply f x n = 
+    if n = 0 then x else apply f (f x) (n-1) 
+  in
 
-    let span file row col = 
-      let f = open_in file in
-        (* scan through rows *)
-        ignore (apply (fun f -> ignore (input_line f); f) f (row-1));
-        let line = input_line f in
-        close_in f;
-        line
-    in
+  (* actual line in the file where the error occured *)
+  let line = 
+    let f = open_in file in
+      (* scan past irrelevant rows  *)
+      ignore (apply (fun f -> ignore (input_line f); f) f (row-1));
+      (* read line where error occured *)
+      let l = input_line f in
+      close_in f; l
+  in
 
+  (* segment line according to where error is *)
+  let pre_error = String.sub line 0 span_s in
+  let error = String.sub line span_s (span_e - span_s) in
+  let post_error = String.sub line span_e (String.length line - span_e) in
 
-    (* pos.Lexing.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol); *)
-    Format.printf "Syntax error in %s on line %d:\n%s"
-      file (row) (span file row col);
-    exit 1
+  (* print color-formatted error message *)
+  ANSITerminal.(print_string [red] "ERROR\n");
+  Format.printf "File %s on line %d:\n" file row;
+  Format.print_flush ();
+  print_string pre_error;
+  ANSITerminal.(print_string [on_red] error);
+  print_string post_error
+
+let eval_error_msg error =
+  ANSITerminal.(print_string [red] "ERROR\n")
 
 let interpret lexbuf =
-
-  (* Parse expression *)
-  Format.printf "Parsing expression...\n";
+  (* parse expression *)
+  Format.printf "\nSytnactic analysis ... ";
   Format.print_flush ();
   let e =
     try
-      ignore (Parsing.set_trace true);
       Parser.program Lexer.token lexbuf
-    with Parsing.Parse_error ->
+    with 
+    | Parsing.Parse_error ->
       syntax_error_msg lexbuf;
       exit 1
+    | End_of_file ->
+      eof_error_msg ();
+      exit 1
   in 
-  
+  (* pretty-print expression *)
+  ANSITerminal.(print_string [green] "GOOD\n");
   e |> Pprint.string_of_e |> print_endline;
 
-  (* TODO: Type-check expression *)
-
-  (* Evaluate expression *)
-  Format.printf "\nEvaluating expression...\n";
+  (* type-check expression TODO *)
+  Format.printf "\nType-checking ... \n";
   Format.print_flush ();
-  let v = Eval.eval e in Pprint.string_of_e v
+
+  (* evaluate expression *)
+  Format.printf "\nRunning program ... ";
+  Format.print_flush ();
+  let v = 
+    try
+      Eval.eval e
+    with Eval.RuntimeError err ->
+      (* TODO: better handling *)
+      eval_error_msg ();
+      print_string err;
+      exit 2
+  in 
+  (* pretty-print result *)
+  ANSITerminal.(print_string [green] "GOOD\n");
+  (* v |> Pprint.string_of_e |> print_endline; *)
+
+  (* return pretty-printed result *)
+  v |> Pprint.string_of_e
 
 let interpret_str str =
   str |> Lexing.from_string |> interpret
